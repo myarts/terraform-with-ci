2ef71fc59cf4b0aad89a1fd188c6486331d94d8b
myarts <myarts.amittidke@gmail.com>
2020-02-21 17:32:02 +0530

    Update ec2.tf

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -2,7 +2,4 @@ resource "aws_instance" "myec2" {
   ami = "ami-0d9462a653c34dab7"
   instance_type = "t3.micro"
   subnet_id = "subnet-0a89b6ab67876cfb9"
-  tags {
-    Managed = "terraformer"
-  }
 }
