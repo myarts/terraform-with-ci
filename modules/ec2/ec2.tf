4b7b856d58c4ca5da7b7318e2195cf09f31465f3
myarts <myarts.amittidke@gmail.com>
2020-02-21 17:28:49 +0530

    Update ec2.tf
    updated instance type

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "myec2" {
   ami = "ami-0d9462a653c34dab7"
-  instance_type = "${lookup(var.instance_type, terraform.workspace)}"
+  instance_type = "t3.micro"
   subnet_id = "subnet-0a89b6ab67876cfb9"
   tags {
     Managed = "terraformer"
